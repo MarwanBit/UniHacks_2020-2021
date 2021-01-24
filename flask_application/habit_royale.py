a713f735aae7053a175e13074bbcc9269a4869f9
marwanbit <marwanbit12@gmail.com>
2021-01-24 12:53:44 -0500

    Added regular formatting

--- flask_application/habit_royale.py
+++ flask_application/habit_royale.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 user1_rooms = [
@@ -17,11 +17,11 @@ user1_rooms = [
 
 @app.route("/")
 def hello():
-    return render_template('root.html')
+    return render_template('root.html', title="Habit Royale HomePage")
 
 @app.route("/user1_homepage")
 def user1_homepage():
-    return render_template('user1_homepage.html', user1_rooms=user1_rooms)
+    return render_template('user1_homepage.html', user1_rooms=user1_rooms, title = "Habit Royale Entrance")
 
 if __name__ == '__main__':
     app.run(debug=True)
