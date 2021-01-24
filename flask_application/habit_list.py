6772c13eaa18bcc51ce6e02fb19ef39b431657c3
marwanbit <marwanbit12@gmail.com>
2021-01-23 21:02:15 -0500

    first two pages added

--- flask_application/habit_list.py
+++ flask_application/habit_list.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello World!</h1>"
-app.run()
+    return "<h1>Hello World :)!</h1>"
+
+@app.route("/user1_homepage")
+def user1_homepage():
+    return "<h1>Welcome user1 :)</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
