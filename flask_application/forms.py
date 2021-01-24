64c9ed0189278976dcfe000b6f76cb258a110c5e
marwanbit <marwanbit12@gmail.com>
2021-01-24 15:11:35 -0500

    Added alot of stuff

---
+++ flask_application/forms.py
@@ -0,0 +1,21 @@
+from habit_royale import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+app.config["SECRET_KEY"] = "d7c8a40871cd9e7f348c0bfaa821dbdc"
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username",
+            validators=[DataRequired(),Length(min=2,max=20)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password",
+                                    validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Log in")
--- flask_application/habit_royale.py
+++ flask_application/habit_royale.py
@@ -1,6 +1,7 @@
 import os
-
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 user1_rooms = [
@@ -22,6 +23,16 @@ def hello():
 @app.route("/user1_homepage")
 def user1_homepage():
     return render_template('user1_homepage.html', user1_rooms=user1_rooms, title = "Habit Royale Entrance")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title="Login",form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
